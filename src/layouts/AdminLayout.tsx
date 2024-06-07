ea9c9af3be26d557016770d260b33221b4af260f
federico-lopez-itti <federico.lopez@itti.digital>
2024-06-07 15:32:40 -0400

    admin layout nav

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -9,17 +9,8 @@ export default function AdminLayout() {
 
 
   return (
-    <div className="flex">
-        <Sidebar />
-      
-      {/* <div className="grid grid-cols-2 gap-4 min-h-screen content-center">
-        <div className="flex flex-col borde-blue-500 justify-center md:px-40">
-          <img className='self-center w-52 mb-8' src={logo} alt="logo" />
-          <Outlet />
-        </div>
-
-      </div> */}
-    </div>
+    <>
+    </>
   );
 }
 
